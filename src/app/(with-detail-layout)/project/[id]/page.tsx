54d44d5ea1c7fa7776c1d5c02df21f56faa683a2
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-02-01 22:04:50 +0900

    minor change to project detail page

--- src/app/(with-detail-layout)/project/[id]/page.tsx
+++ src/app/(with-detail-layout)/project/[id]/page.tsx
@@ -7,9 +7,14 @@ export default function ProjectDetailPage() {
     }
 
     return (
-        <div className="text-white">
-            <h1>{ProjectDetail.title}</h1>
-            <p>{ProjectDetail.description}</p>
+        <div className="text-center">
+            <div className="text-white text-4xl font-bold mt-16 mb-8">
+                <h1>{ProjectDetail.title}</h1>
+            </div>
+            <div className="text-white text-[20px]">
+                <p>{ProjectDetail.description}</p>
+            </div>
         </div>
+        
     )
 }
