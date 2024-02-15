a99f48e59ad739da87092090c539788d2b53eea4
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-02-16 01:24:19 +0900

    email works maybe want to clear the form after sending email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+"use client";
+import React, { useRef, useState } from 'react';
 import LinkedIn from './LinkedIn';
 import GitHub from './Github';
+import emailjs from '@emailjs/browser';
+import { API_KEY } from "../constants/apikeys";
 
 const Contact = () => {
+    const [emailsubmitted, setEmailSubmitted] = useState(false);
+
+    const { service_id, template_id, public_key } = API_KEY[0];
+    const form = useRef<HTMLFormElement>(null);
+
+    const sendEmail = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (form.current) {
+            emailjs
+              .sendForm(service_id, template_id, form.current, {
+                publicKey: public_key,
+                })
+                .then(
+                    () => {
+                        setEmailSubmitted(true);
+                    },
+                    (error) => {
+                        console.log('FAILED...', error.text);
+                    },
+                );
+        }
+    }
+
     return (
         <div className="bg-[#254a8e] pt-[4rem] md:pt-[8rem] pb-[1rem]">
             <h1 className="text-center text-[25px] md:text-[35px] lg:text-[45px] md:leading-[3rem] leading-[2rem] uppercase mb-[3rem] 
@@ -22,35 +49,45 @@ const Contact = () => {
                     <LinkedIn />
                     <GitHub />
                 </div>
-                <div>
+                <form ref={form} onSubmit={sendEmail}>
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-[1rem] items-center">
                         <input 
                           type="text" 
-                          placeholder="Name" 
+                          placeholder="Name"
+                          name="user_name" 
                           className="py-[0.7rem] outline-none text-white bg-gray-800 rounded-md px-4"
                         />
                         <input 
                           type="text" 
-                          placeholder="Email" 
+                          placeholder="Email"
+                          name="user_email" 
                           className="py-[0.7rem] outline-none text-white bg-gray-800 rounded-md px-4"
                         />
                     </div>
                     <input
                         type="text"
                         placeholder="Subject"
+                        name="subject"
                         className="py-[0.7rem] mt-[1rem] w-full outline-none text-white bg-gray-800 rounded-md px-4"
                     />
                     <textarea
                         placeholder="Message"
+                        name="message"
                         className="py-[0.7rem] mt-[1rem] mb-[1.5rem] w-full outline-none text-white bg-gray-800 rounded-md px-4"
                         rows={4}
                     />
-                    <button className="py-[0.7rem] mb-[1.5rem] w-full outline-none text-white bg-blue-700 hover:bg-blue-800 
+                    <button type="submit" className="py-[0.7rem] mb-[1.5rem] w-full outline-none text-white bg-blue-700 hover:bg-blue-800 
                                         rounded-md px-4">
                         Send Message
                     </button>
-
-                </div>
+                    {
+                        emailsubmitted && (
+                            <p className="text-green-500 text-sm mt-2">
+                                Email sent successfully!
+                            </p>
+                        )
+                    }
+                </form>
             </div>
         </div>
     );
