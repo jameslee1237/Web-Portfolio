691da81a511ea3d16dcc171386ba5625f9b30135
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-02-16 19:53:09 +0900

    designing project card + need to reposition

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,12 +5,12 @@ import { projects } from '@/constants';
 
 const Project = () => {
     return (
-        <div className="bg-[#30689f] pt-[4rem] md:pt-[8rem] pb-[12rem]">
+        <div className="bg-[#30689f] pt-[4rem] md:pt-[8rem] pb-[12rem] items-center flex flex-col justify-center">
             <h1 className="text-center text-[25px] md:text-[35px] lg:text-[45px] md:leading-[3rem] leading-[2rem] uppercase mb-[3rem] 
                 font-bold text-white">
                 Project
             </h1>
-            <div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[2rem]">
+            <div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-[8rem] gap-y-[2rem]">
                 {projects.map((project) => (
                     <ProjectCard key={project.id} project={project} />
                 ))}
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,10 +17,10 @@ const ProjectCard = ({
 
     return (
         <div>
-            <div>
-                <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%]
+            <div className="w-[300px] h-[200px] bg-[#ffd700] rounded-md cursor-pointer hover:-translate-y-6 transition-all duration-200 justify-center">
+                <div className="transform relative w-[100%]
                                 h-[200px] md:h-[300px]">
-                    <h1 className="text-[40px] font-bold text-white">
+                    <h1 className="text-[40px] font-bold text-[#1A5176] text-center">
                         <button onClick={handleClick}>{project.image}</button>
                     </h1>
                 </div>   
